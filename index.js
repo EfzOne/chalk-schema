7984fae3631627fb21c63680918bf7a692f573e0
SkyZH <iSkyZH@gmail.com>
2016-06-10 23:51:33 +0800

    add teacher

--- index.js
+++ index.js
@@ -17,16 +17,36 @@ module.exports = (mongoose) => {
     courseId: Number
   });
 
+  var teacherSchema = Schema({
+    teacherId: Number,
+    name: String
+  });
+
+  var orgSchema = Schema({
+    orgId: Number,
+    name: String,
+    class: [Number],
+    teacherId: Number,
+    courseId: Number,
+    times: Number
+  });
+
   var Student = mongoose.model('Student', studentSchema);
   var Type = mongoose.model('Type', typeSchema);
   var Course = mongoose.model('Course', courseSchema);
+  var Teacher = mongoose.model('Teacher', teacherSchema);
+  var Org = mongoose.model('Org', orgSchema);
 
   return {
     studentSchema: studentSchema,
     typeSchema: typeSchema,
     courseSchema: courseSchema,
+    teacherSchema: teacherSchema,
+    orgSchema: orgSchema,
     Student: Student,
     Type: Type,
-    Course: Course
+    Course: Course,
+    Teacher: Teacher,
+    Org: Org
   };
 };
